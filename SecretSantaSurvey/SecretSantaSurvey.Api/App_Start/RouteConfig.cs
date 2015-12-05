c91981f6ca5ad10b2f5d5b157d1e53de02451cca
BaxleyAldworth <jbaxle@gmail.com>
2015-12-05 12:10:50 -0600

    About to make survey controller

--- SecretSantaSurvey/SecretSantaSurvey.Api/App_Start/RouteConfig.cs
+++ SecretSantaSurvey/SecretSantaSurvey.Api/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SecretSantaSurvey.API
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
