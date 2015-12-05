cc7373112bfcaa03f6f3da69413f2c886ee442dd
BaxleyAldworth <jbaxle@gmail.com>
2015-12-05 12:42:19 -0600

    added survery api controller

--- SecretSantaSurvey/SecretSantaSurvey.Api/Models/SecretSantaDB.cs
+++ SecretSantaSurvey/SecretSantaSurvey.Api/Models/SecretSantaDB.cs
@@ -24,6 +24,8 @@ namespace SecretSantaSurvey.API.Models
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
         public virtual DbSet<User> Users { get; set; }
+
+        public System.Data.Entity.DbSet<SecretSantaSurvey.Api.Models.Survey> Surveys { get; set; }
     }
 
     //public class MyEntity
